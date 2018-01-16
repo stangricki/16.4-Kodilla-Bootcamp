e4a9b3e2c0786a35a9c4db8fe047b2eb8bcf4a50
Filip Stangricki <stangricki@gmail.com>
2018-01-16 22:51:35 +0100

    First commit

---
+++ src/containers/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {
+                    text: 'Zadanie 1',
+                    id: 1
+                },
+                {
+                    text: 'Zadanie 2',
+                    id: 2
+                },
+                {
+                    text: 'Zadanie 3',
+                    id: 3
+                }
+
+            ],
+        };
+    }
+    addTodo(val) {
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({
+            data
+        });
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({
+            data: remainder
+        });
+    }
+
+    render() {
+        const list = this.state.data.map((item) => {
+            return (
+                <li key={item.id}>
+                    <p>{item.text}</p>
+                </li>
+            )
+        });
+
+        return (
+            <div className={style.TodoApp}>
+                <Title title="TODO !!!"/>
+                <ul>{list}</ul>
+                <p>Liczba zadań dodanych do listy: {list.length}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
